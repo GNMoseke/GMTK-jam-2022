c8e2aa708674e6d4c5bf4801af432b69c19f597b
Michael <micratajczyk@gmail.com>
2022-07-15 18:59:48 -0500

    Init

---
+++ Assets/Scripts/CreateDice.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateDice : MonoBehaviour
+{
+    public GameObject dice;
+
+    void Start()
+    {
+        //GenerateDice(Random.Range(10, 20));
+        GenerateDice(50);
+    }
+
+    void GenerateDice(int diceNum)
+    {
+        for (int i = 0; i < diceNum; i++)
+        {
+            GameObject newDice = Instantiate(dice, new Vector3(Random.Range(-3.0f, 3.0f), Random.Range(20.0f, 50.0f), Random.Range(-3.0f, 3.0f)), dice.transform.rotation);
+            newDice.name = Random.Range(1, 20).ToString();
+        }
+    }
+}

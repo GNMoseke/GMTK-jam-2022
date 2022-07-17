2b7381d120b736a3acbf9071125ac997ca396caf
Michael <micratajczyk@gmail.com>
2022-07-17 00:34:46 -0500

    Dice Spawn

--- Assets/Scripts/DiceTeleporter.cs
+++ Assets/Scripts/DiceTeleporter.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class DiceTeleporter : MonoBehaviour
 {
+    public GameObject dice;
+
     void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag == "Die")
         {
-            collision.gameObject.transform.position = DiceManager.standardSpawnPosition;
-            collision.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GameObject.Destroy(collision.gameObject);
+            GameObject newDice = Instantiate(dice, new Vector3(38.0f, 25.0f, 15.0f), dice.transform.rotation);
+            int num = Random.Range(2, 20);
+            newDice.GetComponent<DieModel>().value = num;
+            newDice.name = num.ToString();
+            newDice.GetComponent<Renderer>().material.SetFloat("_Number", num);
+            newDice.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-25.0f, -15.0f), 0.0f, Random.Range(-20.0f, -10.0f));
         }
         if (collision.gameObject.tag == "Ticket")
         {

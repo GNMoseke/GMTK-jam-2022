cf14c1ecd5034b65a1bc6dd65d88666e023649bb
Garrett Moseke <>
2022-07-16 19:41:53 -0600

    feat: add ticket rotation on pick up

--- Assets/Scripts/DragDice.cs
+++ Assets/Scripts/DragDice.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class DragDice : MonoBehaviour
 {
     public float forceAmount = 500;
+    public float ticketRotationSpeed = 2f;
 
     Rigidbody selectedRigidbody;
     Camera targetCamera;
@@ -41,6 +42,16 @@ public class DragDice : MonoBehaviour
             Vector3 mousePositionOffset = targetCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, selectionDistance)) - originalScreenTargetPosition;
             mousePositionOffset.y = 2.0f;
             selectedRigidbody.velocity = (originalRigidbodyPos + mousePositionOffset - selectedRigidbody.transform.position) * forceAmount * Time.deltaTime;
+            if (selectedRigidbody.gameObject.tag == "Ticket")
+            {
+                Vector3 currentTicketAngle = selectedRigidbody.gameObject.transform.eulerAngles;
+                Vector3 newTicketAngle = new Vector3(
+                        Mathf.LerpAngle(currentTicketAngle.x, 0f, Time.deltaTime * ticketRotationSpeed),
+                        Mathf.LerpAngle(currentTicketAngle.y, 0f, Time.deltaTime * ticketRotationSpeed),
+                        Mathf.LerpAngle(currentTicketAngle.z, 0f, Time.deltaTime * ticketRotationSpeed)
+                    );
+                selectedRigidbody.gameObject.transform.eulerAngles = newTicketAngle;
+            }
         }
     }
 

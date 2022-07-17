21d8f8257228968ed5e5950526bd466235c06f85
Garrett Moseke <>
2022-07-16 23:06:13 -0600

    fix: randomize tickets

--- Assets/Scripts/GameModel.cs
+++ Assets/Scripts/GameModel.cs
@@ -43,6 +43,8 @@ public class GameModel : MonoBehaviour
     void Start()
     {
         tickets = TicketParser.ReadTickets(ticketsCSV);
+        // randomize ticket order
+        Shuffle(tickets);
         day = 0;
         followerCount = 3;
         UpdateUI();
@@ -155,5 +157,19 @@ public class GameModel : MonoBehaviour
         followerText.text = followerCount.ToString();
         // TODO: scale better
         //followerSlider.value = (float)followerCount / 100f;
+    }
+
+    private static System.Random rng = new System.Random();  
+
+    public static void Shuffle<T>(IList<T> list)  
+    {  
+        int n = list.Count;  
+        while (n > 1) {  
+            n--;  
+            int k = rng.Next(n + 1);  
+            T value = list[k];  
+            list[k] = list[n];  
+            list[n] = value;  
+        }  
     }
 }
